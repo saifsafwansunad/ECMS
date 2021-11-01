8d81a84159dab256ca9e25f0d11bc131027e7222
S_andr0id++ <sunadraj4539@gmail.com>
2021-11-01 19:36:35 +0530

    dummy notification

--- app/src/main/java/com/example/ecms/ui/MyService.java
+++ app/src/main/java/com/example/ecms/ui/MyService.java
@@ -1,19 +1,26 @@
 package com.example.ecms.ui;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.core.app.NotificationCompat;
+
 import com.example.ecms.ApiClient;
 import com.example.ecms.ApiRequests.ToAttendMeetingRequest;
 import com.example.ecms.ApiResponse.ToAttendMeetingResponse;
 import com.example.ecms.DetectConnection;
 import com.example.ecms.LoginActivity;
+import com.example.ecms.Notification;
 import com.example.ecms.PreferenceUtils;
 import com.example.ecms.ToAttendMeetingActivity;
 import com.google.android.material.snackbar.Snackbar;
@@ -25,6 +32,8 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MyService extends Service {
+    private String CHANNEL_ID = "My Notification";
+
     protected Handler handler;
 
     public MyService() {
@@ -91,6 +100,26 @@ public class MyService extends Service {
                                 List<ToAttendMeetingResponse> meetingsList = response.body();
                                 int size=meetingsList.size();
                                 Log.d("key of the message", "size " + size);
+
+                                Intent intentNotification = new Intent(MyService.this, ToAttendMeetingActivity.class);
+                                PendingIntent pendingIntent = PendingIntent.getActivity(MyService.this, 0, intentNotification, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                                NotificationCompat.Builder builderNotificationCompat = new NotificationCompat.Builder(MyService.this, CHANNEL_ID)
+                                        .setContentIntent(pendingIntent)
+                                        .setContentTitle("My Notification")
+                                        .setContentText("My Notification text here.")
+                                        .setSmallIcon(android.R.drawable.btn_star_big_on);
+
+                                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                                NotificationChannel notificationChannel = null;
+                                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                                    notificationChannel = new NotificationChannel(CHANNEL_ID, "This is my first Notification", NotificationManager.IMPORTANCE_DEFAULT);
+                                }
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                    notificationManager.createNotificationChannel(notificationChannel);
+                                }
+                                notificationManager.notify(0,builderNotificationCompat.build());
 
 
 
