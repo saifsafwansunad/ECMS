9b5b73bf9f9947841eaa42c0009c6456e7626013
safwa <saifsafwansunand@gmail.com>
2022-02-01 14:01:04 +0530

    calendars

--- app/src/main/java/com/ecms/ndmecms/Activity/OrganisationCalendarActivity.java
+++ app/src/main/java/com/ecms/ndmecms/Activity/OrganisationCalendarActivity.java
@@ -3,14 +3,32 @@ package com.ecms.ndmecms.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ecms.ndmecms.R;
 
 public class OrganisationCalendarActivity extends AppCompatActivity {
+    TextView title;
+    ImageView backarrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organisation_calendar);
+
+        getSupportActionBar().hide();
+        title=findViewById(R.id.title);
+
+        title.setText("Organisation Calendar");
+        backarrow=findViewById(R.id.imgBackArrow);
+
+        backarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/ecms/ndmecms/Activity/YourCalendarActivity.java
+++ app/src/main/java/com/ecms/ndmecms/Activity/YourCalendarActivity.java
@@ -3,14 +3,31 @@ package com.ecms.ndmecms.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ecms.ndmecms.R;
 
 public class YourCalendarActivity extends AppCompatActivity {
+    TextView title;
+    ImageView backarrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_calendar);
+        getSupportActionBar().hide();
+        title=findViewById(R.id.title);
+
+        title.setText("Your Calendar");
+        backarrow=findViewById(R.id.imgBackArrow);
+
+        backarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
